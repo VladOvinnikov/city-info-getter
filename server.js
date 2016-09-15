86109806e22880a5bd9763865260a32f46f43c27
Vlad Ovinnikov <vlad@Vlads-MacBook-Pro.local>
2016-09-15 06:16:37 +0200

    server.js changed

--- server.js
+++ server.js
@@ -8,7 +8,12 @@
 
     var express = require('express'),
         app = express();
-    app.use(express.static(__dirname + '/app'));
+    // app.use(express.static(__dirname + '/app'));
+    app.use(express.static(path.join(__dirname, 'app')));
+    
+    app.get('*', function (req, res) {
+        res.sendFile("/app/index.html", {"root": __dirname});
+    });
     app.listen(process.env.PORT || 3000);
 
 }());
